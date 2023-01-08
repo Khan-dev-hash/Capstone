5b2e91b427723da5461160aa35cf1df1ea1aa3cd
Khan <Haniffv94@gmail.com>
2023-01-08 14:47:09 -0600

    Add project files.

---
+++ Capstone.Website/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Capstone.Website.Models;
+using Capstone.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Capstone.Website.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        //[HttpPatch] Incase I need to update database ["FromBody"]
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get([FromQuery] string ProductId, int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+                return Ok();
+        }
+    }
+
+}
